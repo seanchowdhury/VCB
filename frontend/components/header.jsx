d8747db0725bd18e53a5ebf1c824fd7d372f592f
Sean Chowdhury <seanchowdhury@gmail.com>
2017-10-26 18:44:46 -0400

    redesigned header

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -3,26 +3,39 @@ import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import values from 'lodash/values';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
   constructor(props) {
     super(props)
+    this.location = ""
+    this.navItems = [
+      { title: "Brooklyn!", path: "/brooklyn"},
+      { title: "SOS!", path: "/sos"},
+      { title: "Blog", path: "/posts"},
+      { title: "About Us", path: "/about"},
+      { title: "Calendar", path: "/calendar"},
+      { title: "FAQ", path: "/faq"},
+      { title: "Download", path: "/download"},
+      { title: "Donate", path: "/donate"},
+      { title: "Groups", path: "/groups"},
+      { title: "Rentals", path: "/rentals"}
+    ]
   }
 
   render() {
+    this.location = this.props.match.path
+    const navItems = this.navItems.map((item, idx) => {
+      let itemClass = 'nav-item'
+      if (this.props.match.path == item.path) {
+        itemClass = 'nav-item nav-selected'
+        console.log(item.title)
+      }
+      return <Link to={item.path} key={idx} className={itemClass}>{item.title}</Link>
+    })
     return (
       <div id='header-container'>
         <ul id='header-nav'>
           <Link to='/posts'><img id='header-logo' src={window.images.vcb_logo} /></Link>
-          <li className='nav-item'>Brooklyn!</li>
-          <li className='nav-item'>SOS!</li>
-          <Link to='/posts'><li className='nav-item'>Blog</li></Link>
-          <li className='nav-item'>About Us</li>
-          <li className='nav-item'>Calendar</li>
-          <li className='nav-item'>FAQ</li>
-          <li className='nav-item'>Download</li>
-          <li className='nav-item'>Donate</li>
-          <li className='nav-item'>Groups</li>
-          <li className='nav-item'>Rentals</li>
+          {navItems}
         </ul>
         <img src={window.images.header} id='header-banner'/>
       </div>
@@ -36,3 +49,5 @@ const mapStateToProps = ({posts}, {match}) => {
 const mapDispatchToProps = (dispatch) => {
 
 };
+
+export default withRouter(Header)
